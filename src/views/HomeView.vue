6379e4c0cccf22869be892b0f531e5b7ed1a38fb
Gerson Borja <gerson.borja2018@gmail.com>
2023-06-29 07:23:23 -0600

    Se agrego enfoque automatico

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -126,6 +126,7 @@ const initCameraAndCaptureImage = () => {
         video: {
             width: { ideal: 1920 }, // ancho deseado
             height: { ideal: 1080 }, // altura deseada
+            focusMode: 'continuous',
             facingMode: 'environment' // 'environment' para la cámara trasera
         }
     }

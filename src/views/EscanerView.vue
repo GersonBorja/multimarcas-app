5b8bb70905a04a72cb91e8bef9fbba40c1401852
Gerson Borja <gerson.borja2018@gmail.com>
2023-07-08 22:41:50 -0600

    Se agrego el mensaje que el envio de correos esta para 32 salas

--- src/views/EscanerView.vue
+++ src/views/EscanerView.vue
@@ -5,54 +5,63 @@
     </div>
   </template>
   
-  <script setup>
-  import { ref, onMounted, onUnmounted } from 'vue';
-  import { BarcodeFormat, BrowserMultiFormatReader, NotFoundException } from '@zxing/library';
-  
-  const video = ref(null);
-  let scanning = ref(false);
-  
-  // Especifica los formatos de códigos de barras que deseas soportar
-  const formats = [
-    BarcodeFormat.CODE_128,
-    BarcodeFormat.CODE_39,
-    BarcodeFormat.EAN_13,
-    BarcodeFormat.EAN_8,
-    BarcodeFormat.UPC_A,
-    BarcodeFormat.UPC_E
-  ];
-  
-  let codeReader = new BrowserMultiFormatReader(null, formats);
-  
-  onMounted(() => {
-    startScanning();
-  });
-  
-  onUnmounted(() => {
-    stopScanning();
-  });
-  
-  const startScanning = () => {
-    scanning.value = true;
-    codeReader
-      .decodeFromVideoDevice(undefined, video.value, (result, err) => {
-        if (result) {
-          alert('Resultado obtenido: ' + result.getText());
-          // Detener el escaneo después de obtener el resultado
-          //stopScanning();
-        }
-        if (err && !(err instanceof NotFoundException)) {
-          console.error(err);
-          // Detener el escaneo si se produce un error
-          stopScanning();
-        }
-      })
-      .catch(err => console.error(err));
-  };
-  
-  const stopScanning = () => {
-    codeReader.reset();
-    scanning.value = false;
-  };
-  </script>
-  
+ <script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+import { BarcodeFormat, BrowserMultiFormatReader, NotFoundException } from '@zxing/library';
+
+const video = ref(null);
+let scanning = ref(false);
+
+// Especifica los formatos de códigos de barras que deseas soportar
+const formats = [
+  BarcodeFormat.CODE_128,
+  BarcodeFormat.CODE_39,
+  BarcodeFormat.EAN_13,
+  BarcodeFormat.EAN_8,
+  BarcodeFormat.UPC_A,
+  BarcodeFormat.UPC_E
+];
+
+let codeReader = new BrowserMultiFormatReader(null, formats);
+
+const constraints = {
+  video: {
+    width: { ideal: 1920 }, // ancho deseado
+    height: { ideal: 1080 }, // altura deseada
+    sharpness: { ideal: 1.0 },
+    focusMode: 'continuous',
+    facingMode: 'environment' // 'environment' para la cámara trasera
+  }
+};
+
+onMounted(() => {
+  startScanning();
+});
+
+onUnmounted(() => {
+  stopScanning();
+});
+
+const startScanning = () => {
+  scanning.value = true;
+  codeReader
+    .decodeFromVideoDevice(undefined, video.value, (result, err) => {
+      if (result) {
+        alert('Resultado obtenido: ' + result.getText());
+        // Detener el escaneo después de obtener el resultado
+        //stopScanning();
+      }
+      if (err && !(err instanceof NotFoundException)) {
+        console.error(err);
+        // Detener el escaneo si se produce un error
+        stopScanning();
+      }
+    }, constraints)
+    .catch(err => console.error(err));
+};
+
+const stopScanning = () => {
+  codeReader.reset();
+  scanning.value = false;
+};
+</script>

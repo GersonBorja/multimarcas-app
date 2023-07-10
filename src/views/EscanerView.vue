a09042690e4a6d543e5b9259fa9f02684caa53bd
Gerson Borja <gerson.borja2018@gmail.com>
2023-07-10 08:22:33 -0600

    Se agrego el mensaje que el envio de correos esta para 32 salas

--- src/views/EscanerView.vue
+++ src/views/EscanerView.vue
@@ -26,8 +26,6 @@ let codeReader = new BrowserMultiFormatReader(null, formats);
 
 const constraints = {
   video: {
-    width: { ideal: 1920 }, // ancho deseado
-    height: { ideal: 1080 }, // altura deseada
     sharpness: { ideal: 1.0 },
     focusMode: 'continuous',
     facingMode: 'environment' // 'environment' para la cámara trasera

7676298c920a8b9912e41e7a0a11cf1156dd91d1
Gerson Borja <gerson.borja2018@gmail.com>
2023-07-14 11:00:56 -0600

    Se agrego mensaje de actualizacion

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -124,8 +124,8 @@ const initCameraAndCaptureImage = () => {
     scan.value = true
     const constraints = {
         video: {
-            //width: { ideal: 1920 }, // ancho deseado
-            //height: { ideal: 1080 }, // altura deseada
+            width: { ideal: 1920 }, // ancho deseado
+            height: { ideal: 1080 }, // altura deseada
             sharpness: { ideal: 1.0 },
             focusMode: 'continuous',
             facingMode: 'environment' // 'environment' para la cámara trasera

4015861774757702b6c4c60760ef213b226e7b0c
Gerson Borja <gerson.borja2018@gmail.com>
2023-07-23 22:23:39 -0600

    se agrgego sonido al escanear

--- src/views/CrearCintillosView.vue
+++ src/views/CrearCintillosView.vue
@@ -63,6 +63,7 @@ const startScanner = () => {
         if (res) {
           try{
             const { data } = await axios.get(`https://procter.work/api/buscador/${res.text}`)
+            audioPlayer.volume = 0.7
             audioPlayer.play()
             barra.value = res.text
             if(data.length === 0){

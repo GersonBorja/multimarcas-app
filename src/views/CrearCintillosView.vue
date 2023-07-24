941ad1d8d5a2e2c524338532327112be9a0dc3fb
Gerson Borja <gerson.borja2018@gmail.com>
2023-07-23 22:05:31 -0600

    se agrgego sonido al escanear

--- src/views/CrearCintillosView.vue
+++ src/views/CrearCintillosView.vue
@@ -56,20 +56,15 @@ onMounted(() => {
       });
 });
 
-const soundScanner = () {
-  const audio = new Audio('../../public/beep.ogg')
-  audio.volume = 0.8
-  return audio
-}
 
 const startScanner = () => {
   scan.value = true;
-  const beep = soundScanner()
     codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', async (res, err) => {
         if (res) {
           try{
             const { data } = await axios.get(`https://procter.work/api/buscador/${res.text}`)
-            beep.play()
+            const audio = new Audio('../../public/beep.ogg');
+            audio.volume = 0.8;
             barra.value = res.text
             if(data.length === 0){
               descripcion.value = ''

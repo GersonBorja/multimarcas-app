3c1510c9bc60aacad2d13775349d393c176b298f
Gerson Borja <gerson.borja2018@gmail.com>
2023-09-21 08:59:31 -0600

    eventos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -67,7 +67,7 @@ onMounted(() => {
        <font-awesome-icon :icon="['fas', 'earth-americas']" />
     </h1>
     <p class="px-4 text-gray-800 text-sm">Cambios de precio de este dia, puedes descargar el documento.</p>
-  <a href="../../public/CAMBIOS-DE-PRECIOS-21-09-23.xlsx" class="flex items-center justify-between bg-gray-100 border border-solid border-gray-200 font-medium m-4 p-4 text-sm text-gray-500" download>CAMBIOS-DE-PRECIOS-21-09-23.xlsx <font-awesome-icon :icon="['fas', 'download']" /></a>
+  <a href="https://cintillos-plazamundo.netlify.app/CAMBIOS-DE-PRECIOS-21-09-23.xlsx" class="flex items-center justify-between bg-gray-100 border border-solid border-gray-200 font-medium m-4 p-4 text-sm text-gray-500" download>CAMBIOS-DE-PRECIOS-21-09-23.xlsx <font-awesome-icon :icon="['fas', 'download']" /></a>
 </div>
   <div class="grid grid-cols-1 sm:grid-cols-2">
     <div
